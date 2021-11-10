fce1de4b4b45246f7990782c2c6b68d9629e1ea6
Muhamad Adi Nursalam <adinursalam21@gmail.com>
2021-11-10 20:22:18 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,49 @@
-# data yang dimasukan pasti berupa string
-data = input("masukan data : ")
-print("data = ",data, ",type =", type(data))
+# casting adalah merubah tipe data satu ke tipe data yang lain
 
-# jika ingin mengambil data integer, maka
-angka = float(input("masukan angka : "))
-angka = int(input("masukan angka : "))
-print("data = ",angka, ",type ="), type(angka))
+#integer
+print("====INTEGER===")
+data_int = 20
+print("data = ", data_int, "type =", type(data_int))
 
-# jika datanya boolean, maka
-biner = bool(float(input("masukan nilai boolean : ")))
-print("data =",biner, ",type = ", type(biner))
+data_float = float(data_int)
+data_str = str(data_int)
+data_bool = bool(data_int)
+print("data = ", data_float, "type =", type(data_float))
+print("data = ", data_str, "type =", type(data_str))
+print("data = ", data_bool, "type =", type(data_bool))
+
+#float
+print("===FLOAT===")
+data_float = 20.7
+print("data = ", data_float, "type =", type(data_float))
+
+data_int = int(data_float)
+data_str = str(data_float)
+data_bool = bool(data_float)
+print("data = ", data_int, "type =", type(data_int))
+print("data = ", data_str, "type =", type(data_str))
+print("data = ", data_bool, "type =", type(data_bool))
+
+#string
+print("===STRING===")
+data_str = 0
+print("data = ", data_str,"type =", type(data_str))
+
+data_int = int(data_str) #STRING HARUS BERUPA ANGKA
+data_float = float(data_str) 
+data_bool = bool(data_str) #STRING AKAN FALSE JIKA KOSONG
+print("data =", data_int, "type =", type(data_int))
+print("data =", data_float, "type =", type(data_float))
+print("data =", data_bool, "type =", type(data_bool))
+
+#boolean
+print("===BOOLEAN====")
+data_bool = True
+print("data =", data_bool, "type =", type(data_bool))
+
+data_int = int(data_bool)
+data_float = float(data_bool)
+data_str = str(data_bool)
+print("data =", data_int, "type =", type(data_int))
+print("data =", data_float, "type =", type(data_float))
+print("data =", data_str, "type =", type(data_str))
