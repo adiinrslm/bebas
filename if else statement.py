120083975d7d3fdd7c515740adf8f008b7cdeb94
unknown <adinursalam21@gmail.com>
2021-12-04 16:01:46 +0700

    first commit for if else

---
+++ if else statement.py
@@ -0,0 +1,21 @@
+# if and else statement
+
+# if
+print("===IF===")
+score = int(input("Enter your score: "))
+
+if score >= 75:
+     print("Congratulations you have passed the exam")
+if score < 75:
+     print("Sorry, you have failed the exam")
+     print("Don\'t give up, you can try again")
+
+# if and else
+print("\n===IF AND ELSE===")
+score = int(input("Enter your score: "))
+
+if score >= 75:
+     print("Congratulations you have passed the exam")
+else:
+     print("Sorry, you have failed the exam")
+     print("Don\'t give up, you can try again")
