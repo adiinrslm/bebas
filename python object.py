8f44d1eaa6813793416cb17cd3a999599df0feda
unknown <adinursalam21@gmail.com>
2022-02-03 20:25:29 +0700

    object in python

---
+++ python object.py
@@ -0,0 +1,40 @@
+# The type() function
+numbers = [1, 4, 9, 16]
+print(type(numbers))
+
+# other objects:
+numbers = [1, 4, 9, 16]
+print(type(numbers))
+
+n1 = 5
+print(type(n1))
+
+flag = True
+print(type(True))
+
+def my_function():
+    pass
+
+print(type(my_function)
+
+# The dir() function
+numbers_list = [1, 2]
+print(dir(numbers_list))
+
+# __add__ method is used to append all the items 
+# of another list to the end of the list.
+numbers_list = [1, 2]
+
+print(dir(numbers_list))
+
+result = numbers_list.__add__([3, 4])
+print(result)
+
+
+# The id() function
+number1 = 5
+print(id(number1))
+
+number2 = 10
+print(id(number2))
+
