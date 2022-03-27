3f796a39d573f6e87bf2663037c75b0b2625e720
unknown <adinursalam21@gmail.com>
2022-03-27 16:05:38 +0700

    colored text in python

---
+++ colored text.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.CYAN+Back.WHITE+"Hi My name is Adi Nursalam "+ Fore.WHITE+ Back.BLUE+"I am learning python programming")
+print(Back.RED+"Hi My name is Adi")
+print(Fore.GREEN + Back.WHITE + Style.DIM + "Hi My name is Adi")
